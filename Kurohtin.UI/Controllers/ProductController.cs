a49f5fe5e17faee253dd5a1e8aaf21ed7a577ddc
Usser <Usser@USERPC>
2024-06-03 21:40:31 +0300

    Add project files.

---
+++ Kurohtin.UI/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Kurohtin.UI.Services.ProductService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kurohtin.UI.Controllers
+{
+    public class ProductController(IProductService categoryService,IProductService productService) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var productResponse =
+            await productService.GetProductListAsync(null);
+            if (!productResponse.Success)
+            return NotFound(productResponse.ErrorMessage);
+            return View(productResponse.Data.Items);
+        }
+    }
+}
